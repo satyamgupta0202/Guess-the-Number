f0de5947df97db801774e801e4e01385149c86fc
satyam <2019335@iiitdmj.ac.in>
2021-05-17 14:30:45 +0530

    Logic Set

--- script.js
+++ script.js
@@ -17,12 +17,33 @@
 // Web Game Main code
 
 const secretNum = Math.trunc(Math.random() * 20) + 1;
-document.querySelector('.number').textContent = secretNum;
-
+//document.querySelector('.number').textContent = secretNum;
+let score = 20;
+let highscore = 0;
 document.querySelector('.check').addEventListener('click', function() {
     const guess = Number(document.querySelector('.guess').value);
     console.log(guess, typeof guess);
     if (!guess) {
         document.querySelector('.message').textContent = 'Please Enter The number';
+    } else if (guess === secretNum) {
+        document.querySelector('.message').textContent = 'You Won!!';
+        document.querySelector('body').style.backgroundColor = '#60b347';
+        document.querySelector('.number').style.width = '30rem';
+    } else if (guess > secretNum) {
+        if (score > 1) {
+            score--;
+            document.querySelector('.message').textContent = 'Guess Too high!!';
+            document.querySelector('.score').textContent = score;
+        } else {
+            document.querySelector('.message').textContent = 'Game Over';
+        }
+    } else if (guess < secretNum) {
+        if (score > 1) {
+            score--;
+            document.querySelector('.message').textContent = 'Guess Too low!!';
+            document.querySelector('.score').textContent = score;
+        } else {
+            document.querySelector('.message').textContent = 'Game Over';
+        }
     }
 });
